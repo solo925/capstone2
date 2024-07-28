b9311df4f556085b3f02bc410a1393b52e60891c
davinci <davinci@localhost.localdomain>
2024-07-28 10:33:42 +0300

    created the powerpoints app

--- powerpoint/models.py
+++ powerpoint/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class PowerPoint(models.Model):
+    title = models.CharField(max_length=100)
+    file = models.FileField(upload_to='powerpoints/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
--- powerpoint/views.py
+++ powerpoint/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required, user_passes_test
+from .models import PowerPoint
+from .forms import PowerPointForm
 
-# Create your views here.
+# Check if the user is an admin
+def is_admin(user):
+    return user.is_superuser
+
+@login_required
+def powerpoint_list(request):
+    powerpoints = PowerPoint.objects.all()
+    return render(request, 'powerpoint/powerpoint_list.html', {'powerpoints': powerpoints})
+
+@user_passes_test(is_admin)
+def powerpoint_upload(request):
+    if request.method == 'POST':
+        form = PowerPointForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('powerpoint_list')
+    else:
+        form = PowerPointForm()
+    return render(request, 'powerpoint/powerpoint_upload.html', {'form': form})
+
